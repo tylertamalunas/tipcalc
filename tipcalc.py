b4d5e827b7c718c124c862aa1548ac0ba4820cc1
tylertamalunas <46872798+tylertamalunas@users.noreply.github.com>
2019-01-30 13:06:48 -0600

    Add files via upload
    basic tip calculator for predetermined amounts

---
+++ tipcalc.py
@@ -0,0 +1,12 @@
+#meal variables
+meal_cost = 44.50
+# tax in percent
+tax = .0675
+# tip in percent
+tip = .15
+# cost of meal and tax
+meal_cost = meal_cost + meal_cost * tax
+#total cost of meal + tip and tax
+total = meal_cost + meal_cost * tip
+
+print(total)
